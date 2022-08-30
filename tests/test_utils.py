9a6b582e0507162f8ab74eadb6dc7e72c4a27373
Markas Klapovščiuk <48568793+markkkkas@users.noreply.github.com>
2022-08-30 11:58:55 +0300

    Update test_utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -7,14 +7,14 @@ from coingate.utils import date_to_str_or_none
 
 class TestUtils:
     @pytest.mark.parametrize(
-        "date,expectedType", [(datetime(2022, 10, 10, 10, 12, 11), str), (None, None)]
+        "date,expected_type", [(datetime(2022, 10, 10, 10, 12, 11), str), (None, None)]
     )
     def test_date_to_str_or_none(
-        self, date: Optional[datetime], expectedType: Optional[str]
+        self, date: Optional[datetime], expected_type: Optional[str]
     ):
         value = date_to_str_or_none(date)
 
-        if expectedType is not None:
-            assert type(value) == expectedType
+        if expected_type is not None:
+            assert type(value) == expected_type
         else:
             assert value is None
