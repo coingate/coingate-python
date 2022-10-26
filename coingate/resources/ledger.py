7e07008c0eb5d29b73f1cb69839d225a61b64a71
Markas Klapovščiuk <klapovsciuk@gmail.com>
2022-10-26 15:29:45 +0200

    feat: Implement LedgerAccountCurrency, LedgerAccount and PaginatedLedgerAccounts resources

---
+++ coingate/resources/ledger.py
@@ -0,0 +1,24 @@
+from typing import List
+
+from pydantic import BaseModel
+
+
+class LedgerAccountCurrency(BaseModel):
+    id: int
+    title: str
+    symbol: str
+
+
+class LedgerAccount(BaseModel):
+    id: str
+    balance: str
+    status: str
+    currency: LedgerAccountCurrency
+
+
+class PaginatedLedgerAccounts(BaseModel):
+    current_page: int
+    per_page: int
+    total_accounts: int
+    total_pages: int
+    accounts: List[LedgerAccount]
